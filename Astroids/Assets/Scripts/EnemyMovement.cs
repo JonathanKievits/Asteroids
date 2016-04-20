eea6166d529d30bb21406023984264b9addd801f
JonathanKievits <Jonathan.kievits@hotmail.nl>
2016-04-20 13:28:06 +0200

    Update 2.0 20-4-2016
    Enemy ai/spawning

---
+++ Astroids/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    public float rotationSpeed;
+    public float movementSpeed;
+    public float rotationTime;
+
+    private float randomTime;
+
+    void Start()
+    {
+        Invoke("ChangeRotation", rotationTime);
+    }
+
+    void ChangeRotation()
+    {
+        if (Random.value > 0.5f)
+        {
+            rotationSpeed = -rotationSpeed;
+        }
+        Invoke("ChangeRotation", rotationTime);
+    }
+
+
+    void Update()
+    {
+        randomTime = Random.Range(1,4);
+        rotationTime = randomTime;
+        transform.Rotate(new Vector3(0, rotationSpeed, 0 * Time.deltaTime));
+        transform.position += transform.forward * movementSpeed * Time.deltaTime;
+
+    }
+}
